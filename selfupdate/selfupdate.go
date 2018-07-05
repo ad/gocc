93495133f679b86ac5cc9fea47271ac09ccee009
ad <daniel.apatin@gmail.com>
2018-07-05 13:57:30 +0300

    *

--- selfupdate/selfupdate.go
+++ selfupdate/selfupdate.go
@@ -10,15 +10,17 @@ import (
 	"github.com/blang/semver"
 	"github.com/kardianos/osext"
 	"github.com/rhysd/go-github-selfupdate/selfupdate"
+
+	"github.com/ad/gocc/utils"
 )
 
-func StartSelfupdate(slug string, version string) {
+func StartSelfupdate(slug string, version string, fqdn string) {
 	selfUpdateTicker := time.NewTicker(5 * time.Minute)
 	go func(selfUpdateTicker *time.Ticker) {
 		for {
 			select {
 			case <-selfUpdateTicker.C:
-				if err := selfUpdate(slug, version); err != nil {
+				if err := selfUpdate(slug, version, fqdn); err != nil {
 					fmt.Fprintln(os.Stderr, err)
 					// os.Exit(1)
 				}
@@ -27,7 +29,7 @@ func StartSelfupdate(slug string, version string) {
 	}(selfUpdateTicker)
 }
 
-func selfUpdate(slug string, version string) error {
+func selfUpdate(slug string, version string, fqdn string) error {
 	previous := semver.MustParse(version)
 	latest, err := selfupdate.UpdateSelf(previous, slug)
 	if err != nil {
@@ -39,6 +41,9 @@ func selfUpdate(slug string, version string) error {
 	} else {
 		fmt.Println("Update successfully done to version", latest.Version)
 		fmt.Println("Release note:\n", latest.ReleaseNotes)
+
+		go utils.Post("http://127.0.0.1:80/pub/"+fqdn, `{"action": "updated", "version": "`+fmt.Sprint(latest.Version)+`"}`)
+
 		file, err := osext.Executable()
 		if err != nil {
 			return err
