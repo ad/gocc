5f7d018183bae15be310547af3222d210ad05ff0
ad <daniel.apatin@gmail.com>
2018-07-16 21:53:45 +0300

    *

--- updater.sh
+++ updater.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "$1"
 git tag "$1" && git push --tags
-go-bindata -nometadata -pkg main -prefix templates/ -o bindata.go templates/*
+go-bindata -nometadata -pkg main -prefix templates/ -o bindata/bindata.go templates/*
 gox -output="release/{{.Dir}}_{{.OS}}_{{.Arch}}" -osarch="darwin/386 darwin/amd64 linux/386 linux/amd64 linux/arm"
 github-release release --user ad --repo gocc --tag "$1" --name "$1"
 github-release upload --user ad --repo gocc --tag "$1" --name "gocc_darwin_386" --file release/gocc_darwin_386
